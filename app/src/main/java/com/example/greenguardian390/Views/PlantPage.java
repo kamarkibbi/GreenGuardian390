d86bb00e07277ce4363bae10576dc42cb1eff678
kamarkibbi <91108326+kamarkibbi@users.noreply.github.com>
2023-03-16 21:56:54 -0400

    Still working on it. Finished UI of plant page and set it up to receive sensor data from firebase. Still couldnt figure out the backend for add a plant page and main page (stuck).

--- app/src/main/java/com/example/greenguardian390/Views/PlantPage.java
+++ app/src/main/java/com/example/greenguardian390/Views/PlantPage.java
@@ -3,18 +3,58 @@ package com.example.greenguardian390.Views;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 import com.example.greenguardian390.R;
 
 public class PlantPage extends AppCompatActivity {
 
+    TextView plantName,tempSensor,tempInput,soilSensor,soilInput;
+
+    Button deleteButton,helpButton,editButton;
+
+    DatabaseReference mDatabase;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_page);
+
+        plantName=findViewById(R.id.plantName);
+        tempSensor=findViewById(R.id.tempSensor);
+        tempInput=findViewById(R.id.tempInput);
+        soilSensor=findViewById(R.id.soilSensor);
+        soilInput=findViewById(R.id.soilInput);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("sensorData");
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot d :snapshot.getChildren())
+                {
+                    if (d.getKey().toLowerCase().contains("moisture"))
+                        soilSensor.setText((CharSequence) d.getValue());
+                    if(d.getKey().toLowerCase().contains("temperature"))
+                        tempSensor.setText((CharSequence)d.getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
